dc9ad50d77fa58647c4573076d9e00ce29d339e3
insub <vb15@kakao.com>
2024-06-17 23:50:19 +0900

    init

---
+++ Clpetition/ClpetitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClpetitionApp.swift
+//  Clpetition
+//
+//  Created by 김인섭 on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ClpetitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
